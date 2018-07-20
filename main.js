5804ea97b8084167420f2f07df7af92ea479610c
Greg Weber <gregoryweberguitars@gmail.com>
2018-07-19 23:14:07 -0700

    wrote tests

--- main.js
+++ main.js
@@ -3,10 +3,8 @@
 
 class Eventz {
     constructor() {
-        // store named events as key and the handeler functions associated in an array eg. {click : [ handler, handler, ...]}
         this.registeredEvents = {};
     }
-
 
     //register event name and associated handeler 
     register(name, handler) {
@@ -35,8 +33,9 @@ class Eventz {
         });
     }
 
+
     // remove a handeler from a named event
-    remove(name, handeler) {
+    remove(name, handler) {
         if (!this.registeredEvents[name]) {
             return undefined;
         }
@@ -44,7 +43,7 @@ class Eventz {
             delete this.registeredEvents[name];
             return;
         }
-        this.registeredEvents[name] = this.registeredEvents[name].filter(element => element != handeler)
+        this.registeredEvents[name] = this.registeredEvents[name].filter(element => element != handler)
     }
 
     // remove all handelers asociated with a given named event
@@ -58,40 +57,66 @@ class Eventz {
         delete this.registeredEvents[name];
     }
 
+
     // register a onetime event handeler that will be removed upon the event emitting
-    once(name, handeler) {
-        // first we register the event name with a callback that will get triggered upon emission
+    once(name, handler) {
+        if (arguments.length != 2) {
+            throw new Error('name and handler must be provided as a string and function respectively')
+        }
+        if (typeof(name) != 'string' || typeof(handler) != 'function') {
+            throw new TypeError;
+        }
+        // register the event name with a callback that will get triggered upon emission
         this.register(name, callback);
 
-        // now in callback we remove the event from registered events and call the handeler that was first passed
+        // now in callback we remove the event from registered events and call the handler that was first passed
         function callback() {
-            handeler();
+            handler();
             this.remove(name, callback);
         }
     }
-
 }
-
 
 
 
 let ev = new Eventz;
 
-let functonX = function () { console.log('my second listiner') }
+// methods:
+let functonX = () => console.log('my first event');
+let greet = (firstName, lastName) => console.log(`Hello ${firstName} ${lastName}, how are you today?`);
+let custom = () => console.log('this is a custom event');
+let sum = (x, y) => console.log(x + y);
 
+
+
+// registrations: 
 ev.register('click', functonX);
-ev.register('click', function () { console.log('my second listiner', arguments) });
-ev.register('customEvent', function () { console.log('this is a custom event') });
+ev.register('customEvent', custom);
+ev.register('submit', sum);
+ev.register('pageLoad', greet);
 
-ev.emit('click', 'hello', 'world', 'we can have as many', 'arguments as', 'we wish');
+
+// emits
+ev.emit('click', 'hello', 'world', 'we can', 'have zas', 'many', 'arguments as', 'we wish!');
 ev.emit('customEvent');
+ev.emit('submit', 4, 5);
+ev.emit('pageLoad', 'John', 'Doe')
 
 
-// ev.once('hover', () => console.log('hovering one time'));
-// console.log(ev.registeredEvents);
-// ev.emit('hover');
+
+// register one time event:
+ev.once('hover', () => console.log('hovering one time'));
+
+// emit one time event
+ev.emit('hover');
+
+
+
+console.log('********************');
 console.log(ev.registeredEvents);
 ev.remove('click', functonX);
-
-console.log(ev.registeredEvents);
-
+console.log('********************');
+console.log(ev.registeredEvents); 
+console.log('********************');
+ev.removeAll('submit');
+console.log(ev.registeredEvents); 
